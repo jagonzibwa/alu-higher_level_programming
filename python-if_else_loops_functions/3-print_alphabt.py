646dcb4c38e39266e428c090292266ff28625b2a
jagonzibwa <j.agonzibwa@alustudent.com>
2025-03-03 13:00:33 +0200

    3-print_alphabt.py

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for letter in range(ord('a'), ord('z')+1):
-    if chr(letter) != 'e' and chr(letter) != 'q':
-        print('{:c}'.format(letter), end='')
-
+# Author - Tolulope Fakunle
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
