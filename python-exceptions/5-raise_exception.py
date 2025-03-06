df8b814dcd15ea921eaf8668e5bd9cd6f81cbc3f
jagonzibwa <j.agonzibwa@alustudent.com>
2025-03-06 15:55:34 +0200

    5-raise_exception.py

--- python-exceptions/5-raise_exception.py
+++ python-exceptions/5-raise_exception.py
@@ -1,1 +1,10 @@
-nnnn
+#!/usr/bin/python3
+def raise_exception():
+    raise TypeError("Exception raised")
+
+# Test case
+if __name__ == "__main__":
+    try:
+        raise_exception()
+    except TypeError as te:
+        print(te)
