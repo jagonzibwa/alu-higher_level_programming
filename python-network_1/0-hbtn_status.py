ffea610042a3514a949995206a97c0e0e99f6ea2
jagonzibwa <j.agonzibwa@alustudent.com>
2025-03-17 12:06:24 +0200

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,3 +1,19 @@
-#!/bin/bash
-# sends a POST request to the passed URL, and displays the body of the response.
-curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+#!/usr/bin/python3
+"""A script that
+- fetches https://alu-intranet.hbtn.io/status.
+- uses urlib package
+"""
+
+import urllib.request
+
+url = 'https://intranet.hbtn.io/status'
+if url.startswith('https://'):
+    url = 'https://alu-intranet.hbtn.io/status'
+
+if __name__ == '__main__':
+    with urllib.request.urlopen(url) as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
